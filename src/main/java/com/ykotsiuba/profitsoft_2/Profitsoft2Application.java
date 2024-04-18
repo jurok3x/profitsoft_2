24cf04a5011a6a9f82a4964a0a8f826ffc4636c8
ykotsiuba <jurok3x@gmail.com>
2024-04-18 13:14:17 +0300

    First commit

---
+++ src/main/java/com/ykotsiuba/profitsoft_2/Profitsoft2Application.java
@@ -0,0 +1,13 @@
+package com.ykotsiuba.profitsoft_2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Profitsoft2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Profitsoft2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/ykotsiuba/profitsoft_2/TestProfitsoft2Application.java
@@ -0,0 +1,23 @@
+package com.ykotsiuba.profitsoft_2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.springframework.context.annotation.Bean;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.utility.DockerImageName;
+
+@TestConfiguration(proxyBeanMethods = false)
+public class TestProfitsoft2Application {
+
+	@Bean
+	@ServiceConnection
+	PostgreSQLContainer<?> postgresContainer() {
+		return new PostgreSQLContainer<>(DockerImageName.parse("postgres:latest"));
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.from(Profitsoft2Application::main).with(TestProfitsoft2Application.class).run(args);
+	}
+
+}
