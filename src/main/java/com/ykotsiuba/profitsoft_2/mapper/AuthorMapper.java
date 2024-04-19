8722f5aa5220fcdc43eab29cf8dba783699bcd9b
ykotsiuba <jurok3x@gmail.com>
2024-04-19 08:05:13 +0300

    Added mapper.

---
+++ src/main/java/com/ykotsiuba/profitsoft_2/mapper/AuthorMapper.java
@@ -0,0 +1,11 @@
+package com.ykotsiuba.profitsoft_2.mapper;
+
+import com.ykotsiuba.profitsoft_2.dto.AuthorDTO;
+import com.ykotsiuba.profitsoft_2.entity.Author;
+import org.mapstruct.Mapper;
+
+@Mapper(componentModel = "spring")
+public interface AuthorMapper {
+    Author toEntity(AuthorDTO authorDTO);
+    AuthorDTO toDTO(Author author);
+}
