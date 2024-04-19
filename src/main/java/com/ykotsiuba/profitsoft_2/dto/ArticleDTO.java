39aadcef840132bda2a2e9d1ba10cc08a03f1bbf
ykotsiuba <jurok3x@gmail.com>
2024-04-19 08:04:58 +0300

    Updated dto.

--- src/main/java/com/ykotsiuba/profitsoft_2/dto/ArticleDTO.java
+++ src/main/java/com/ykotsiuba/profitsoft_2/dto/ArticleDTO.java
@@ -1,13 +1,17 @@
 package com.ykotsiuba.profitsoft_2.dto;
 
 import com.ykotsiuba.profitsoft_2.entity.Field;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.UUID;
 
 @Data
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class ArticleDTO {
     private UUID id;
     private String title;
--- src/main/java/com/ykotsiuba/profitsoft_2/dto/AuthorDTO.java
+++ src/main/java/com/ykotsiuba/profitsoft_2/dto/AuthorDTO.java
@@ -1,12 +1,16 @@
 package com.ykotsiuba.profitsoft_2.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.UUID;
 
 @Data
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class AuthorDTO {
     private UUID id;
     private String firstName;
