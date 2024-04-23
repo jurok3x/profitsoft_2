e8b4b00a75486cc92079f6be4841f7b0a1cd12a5
yurii kotsiuba <jurok3x@gmail.com>
2024-04-23 18:04:16 +0300

    Added find by email.

--- src/main/java/com/ykotsiuba/profitsoft_2/repository/AuthorRepository.java
+++ src/main/java/com/ykotsiuba/profitsoft_2/repository/AuthorRepository.java
@@ -4,8 +4,11 @@ import com.ykotsiuba.profitsoft_2.entity.Author;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 @Repository
 public interface AuthorRepository extends JpaRepository<Author, UUID> {
+
+    Optional<Author> findByEmail(String email);
 }
--- src/main/java/com/ykotsiuba/profitsoft_2/service/AuthorService.java
+++ src/main/java/com/ykotsiuba/profitsoft_2/service/AuthorService.java
@@ -3,11 +3,15 @@ package com.ykotsiuba.profitsoft_2.service;
 import com.ykotsiuba.profitsoft_2.dto.AuthorDTO;
 import com.ykotsiuba.profitsoft_2.dto.DeleteAuthorResponseDTO;
 import com.ykotsiuba.profitsoft_2.dto.SaveAuthorRequestDTO;
+import com.ykotsiuba.profitsoft_2.entity.Author;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface AuthorService {
     AuthorDTO findById(String id);
+
+    Optional<Author> findByEmail(String email);
 
     List<AuthorDTO> findAll();
 
