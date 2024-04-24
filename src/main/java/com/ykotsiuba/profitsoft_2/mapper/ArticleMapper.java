b98ed8bcc4c8b524cf350d364ea56553b3d33597
ykotsiuba <jurok3x@gmail.com>
2024-04-24 13:28:49 +0300

    Added article response mapping.

--- src/main/java/com/ykotsiuba/profitsoft_2/mapper/ArticleMapper.java
+++ src/main/java/com/ykotsiuba/profitsoft_2/mapper/ArticleMapper.java
@@ -1,7 +1,9 @@
 package com.ykotsiuba.profitsoft_2.mapper;
 
 import com.ykotsiuba.profitsoft_2.dto.ArticleDTO;
+import com.ykotsiuba.profitsoft_2.dto.ArticleResponseDTO;
 import com.ykotsiuba.profitsoft_2.entity.Article;
+import com.ykotsiuba.profitsoft_2.entity.Author;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 
@@ -13,4 +15,12 @@ public interface ArticleMapper {
 
     @Mapping(target = "authorDTO", source = "author")
     ArticleDTO toDTO(Article article);
+
+    @Mapping(target = "authorFullName", expression = "java(mapFullName(article))")
+    ArticleResponseDTO toResponseDTO(Article article);
+
+    default String mapFullName(Article article) {
+        Author author = article.getAuthor();
+        return String.format("%s %s", author.getFirstName(), author.getLastName());
+    }
 }
