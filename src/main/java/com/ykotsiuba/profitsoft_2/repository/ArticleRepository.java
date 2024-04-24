07fa86bca9b4fe286b2372967f577aad852db3bb
ykotsiuba <jurok3x@gmail.com>
2024-04-24 13:28:25 +0300

    Fixed search query.

--- src/main/java/com/ykotsiuba/profitsoft_2/repository/ArticleRepository.java
+++ src/main/java/com/ykotsiuba/profitsoft_2/repository/ArticleRepository.java
@@ -2,23 +2,25 @@ package com.ykotsiuba.profitsoft_2.repository;
 
 import com.ykotsiuba.profitsoft_2.dto.SearchArticleRequestDTO;
 import com.ykotsiuba.profitsoft_2.entity.Article;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
 import java.util.UUID;
 
 @Repository
 public interface ArticleRepository extends JpaRepository<Article, UUID> {
 
-    @Query("SELECT a FROM Article a WHERE (:#{#search.field} is null or a.field = :#{#search.field})"
+    @Query("SELECT a FROM Article a WHERE "
+            + "(:#{#search.field} is null or CAST(a.field as string) = :#{#search.field})"
             + "and (:#{#search.journal} is null or a.journal = :#{#search.journal})"
             + "and (:#{#search.year} is null or a.year = :#{#search.year})"
-            + "and (:#{#search.authorId} is null or a.author = :#{#search.authorId})"
-            + "and (:#{#search.titlePart} is null or a.title ILIKE '%:#{#search.titlePart}%')"
+            + "and (:#{#search.authorId} is null or CAST(a.author.id as string) = :#{#search.authorId})"
+            + "and (:#{#search.titlePart} is null or lower(a.title) like lower(concat('%', :#{#search.titlePart}, '%')))"
     )
-    List<Article> search(@Param("search") SearchArticleRequestDTO searchDTO);
+    Page<Article> search(@Param("search") SearchArticleRequestDTO searchDTO, Pageable pageable);
 
 }
