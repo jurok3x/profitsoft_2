5af4d8b63d8f792ef565d1c800d41d22ffe2b10f
ykotsiuba <jurok3x@gmail.com>
2024-04-25 17:27:47 +0300

    Added generate report controller.

--- src/test/java/com/ykotsiuba/profitsoft_2/controller/ArticleControllerTest.java
+++ src/test/java/com/ykotsiuba/profitsoft_2/controller/ArticleControllerTest.java
@@ -7,7 +7,7 @@ import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import com.ykotsiuba.profitsoft_2.Profitsoft2Application;
 import com.ykotsiuba.profitsoft_2.TestProfitsoft2Application;
-import com.ykotsiuba.profitsoft_2.dto.*;
+import com.ykotsiuba.profitsoft_2.dto.APIException;
 import com.ykotsiuba.profitsoft_2.dto.article.*;
 import com.ykotsiuba.profitsoft_2.entity.Article;
 import com.ykotsiuba.profitsoft_2.repository.ArticleRepository;
