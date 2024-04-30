d958ddbf5490bdd662c70236aa3754da3cbb081b
yurii kotsiuba <jurok3x@gmail.com>
2024-04-30 18:53:01 +0300

    Added javadocs.

--- src/main/java/com/ykotsiuba/profitsoft_2/service/ArticleParserService.java
+++ src/main/java/com/ykotsiuba/profitsoft_2/service/ArticleParserService.java
@@ -1,8 +1,17 @@
 package com.ykotsiuba.profitsoft_2.service;
 
 import com.ykotsiuba.profitsoft_2.dto.article.ParsingResultDTO;
+import com.ykotsiuba.profitsoft_2.exception.FileParsingException;
 import org.springframework.web.multipart.MultipartFile;
 
 public interface ArticleParserService {
+
+    /**
+     * Parses JSON and returns list of validated and not valid articles.
+     *
+     * @param file JSON file with articles data.
+     * @return ParsingResultDTO response containing list valid, and invalid articles.
+     * @throws FileParsingException for parsing error.
+     */
     ParsingResultDTO parse(MultipartFile file);
 }
