a765b1654224e2c60b579355ad9312a5a2bb0a03
yurii kotsiuba <jurok3x@gmail.com>
2024-04-30 19:29:08 +0300

    Added mapper method.

--- src/main/java/com/ykotsiuba/profitsoft_2/mapper/AuthorMapper.java
+++ src/main/java/com/ykotsiuba/profitsoft_2/mapper/AuthorMapper.java
@@ -1,11 +1,17 @@
 package com.ykotsiuba.profitsoft_2.mapper;
 
 import com.ykotsiuba.profitsoft_2.dto.author.AuthorDTO;
+import com.ykotsiuba.profitsoft_2.dto.author.SaveAuthorRequestDTO;
 import com.ykotsiuba.profitsoft_2.entity.Author;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring")
 public interface AuthorMapper {
     Author toEntity(AuthorDTO authorDTO);
+
+    @Mapping(target = "id", ignore = true)
+    Author fromSaveRequestToEntity(SaveAuthorRequestDTO requestDTO);
+
     AuthorDTO toDTO(Author author);
 }
